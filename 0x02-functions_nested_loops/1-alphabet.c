87cd2cc3662a710c3ec346db0a15ff731919ff78
Your Name <you@example.com>
2022-06-07 16:07:19 -0700

    .

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,14 +10,12 @@ int main(void)
  */
 void print_alphabet(void)
 {
-	int i;
+        int i;
 
-	for (i = 97; i <= 122; i++)
-	{
-		_putchar(i);
-	}
-	_putchar('\n');
-	void print_alphabet(void);
-
-
-}
+        for (i = 97; i <= 122; i++)
+        {
+                _putchar(i);
+        }
+        _putchar('\n');
+        void print_alphabet(void);
+}	
